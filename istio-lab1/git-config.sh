e438d3066e277106f1c122820e5a5238c66c5c35
Ivan Ferreira <ivan.ferreira@gmail.com>
2022-08-06 11:46:05 +0000

    Update istio-lab1/git-config.sh

---
+++ istio-lab1/git-config.sh
@@ -0,0 +1,5 @@
+USERNAME="Ivan Ferreira"
+USEREMAIL="ivan.ferreira@gmail.com"
+git config --global  user.name $USER
+git config --global  user.email $USEREMAIL
+git config --global core.editor "vim"
